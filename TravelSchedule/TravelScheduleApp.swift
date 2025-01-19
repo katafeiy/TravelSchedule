6ac33945a06eb4dc00f1f28f1323be289a7866c5
Filippov Konstantin <fkv.katafey@yandex.ru>
2025-01-19 11:22:25 +0400

    Initial Commit

---
+++ TravelSchedule/TravelScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelScheduleApp.swift
+//  TravelSchedule
+//
+//  Created by Константин Филиппов on 19.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct TravelScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
